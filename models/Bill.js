7a82a5760838238f235a9c3c5829d054b9d59c02
21120150 <toancva153@gmail.com>
2024-01-22 22:31:35 +0700

    Change

--- models/Bill.js
+++ models/Bill.js
@@ -2,43 +2,18 @@ const db = require('../utilities/database');
 
 module.exports = {
     selectAllBills: async () => {
-        const rs = await db.any('SELECT * FROM public."HoaDon"');
-        return rs;
+        await db.selectAllBills();
     },
     addTTHoaDon: async (MaHoaDon, obj) => {
-        try {
-            await db.none(
-            'INSERT INTO public."ThongTinHoaDon"("MaHoaDon", "MaSach", "SoLuong") VALUES ($1, $2, $3)',
-            [MaHoaDon, obj.bookId, obj.quantity]
-            );
-        } catch (error) {
-            console.error("Error inserting:", error);
-            throw error;
-        }
+        await db.addTTHoaDon(MaHoaDon, obj);
     },
     insertBill: async (obj) => {
-        await db.none(
-        'INSERT INTO "HoaDon"("username", "NgayLap", "ThanhTien", "TrangThai") VALUES($1, $2, $3, $4)',
-        [obj.username, obj.date, obj.total, obj.status]
-        );
-        
-    },
-    selectHoaDon: async (month, year) => {
-        const rs = await db.any(
-            'SELECT * FROM "HoaDon" WHERE EXTRACT(YEAR FROM "NgayLap") = $1 AND EXTRACT(MONTH FROM "NgayLap") = $2;',
-            [year, month]
-        );
-        return rs;
+        await db.insertBill(obj);
     },
     selectTTHoaDon: async (MaHD) => {
-        const rs = await db.any(
-            'SELECT * FROM "ThongTinHoaDon" WHERE "MaHoaDon" = $1;',
-            [MaHD]
-        );
-        return rs;
+        await db.selectTTHoaDon(MaHD);
     },
     updateStatus: async (id, status) => {
-        const updateQuery = 'UPDATE public."HoaDon" SET "TrangThai" = $1 WHERE "id" = $2';
-        await db.none(updateQuery, [status, id]);
+        await db.updateStatus(id, status);
     }
 };
