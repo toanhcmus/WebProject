ad5e01c46a803bc56b449c0226b6c8790e043af3
21120150 <toancva153@gmail.com>
2024-01-24 23:56:58 +0700

    fix serverpayment

--- serverPayment.js
+++ serverPayment.js
@@ -17,6 +17,8 @@ app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs');
 app.set('views', './views');
 
+const db = require("./utilities/databasePayment");
+
 
 // options for https
 const options = {
@@ -37,7 +39,11 @@ app.get('*', function(req, res){
   res.render("404");
 });
 
-
-server.listen(process.env.PORT_PAYMENT, function() {
-  console.log(`Server PAYMENT started on port https://localhost:${process.env.PORT_PAYMENT}`);
+db.initDatabase().then(() => {
+    server.listen(process.env.PORT_PAYMENT, function() {
+        console.log(`Server PAYMENT started on port https://localhost:${process.env.PORT_PAYMENT}`);
+    });
+}).catch(err => {
+    console.error(`Failed to initialize database: ${err}`);
 });
+
